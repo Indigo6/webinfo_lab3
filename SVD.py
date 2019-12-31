ba7bf149275a8dbbbee67b5784910800ac9e5394
clivefang <clivefang@tencent.com>
2019-12-31 17:25:19 +0800

    add grid-search

--- SVD.py
+++ SVD.py
@@ -1,13 +1,30 @@
 import pandas as pd
 import numpy as np
+import time
+import pickle
+
 from surprise import SVDpp, SVD, accuracy
 from surprise import Dataset, Reader
 from surprise.model_selection import cross_validate, train_test_split
-import pickle
+
+
+def fmt_time(dtime):
+    if dtime <= 0:
+        return '0:00.000'
+    elif dtime < 60:
+        return '0:%02d.%03d' % (int(dtime), int(dtime * 1000) % 1000)
+    elif dtime < 3600:
+        return '%d:%02d.%03d' % (int(dtime / 60), int(dtime) % 60, int(dtime * 1000) % 1000)
+    else:
+        return '%d:%02d:%02d.%03d' % (int(dtime / 3600), int((dtime % 3600) / 60), int(dtime) % 60,
+                                      int(dtime * 1000) % 1000)
 
 
 if __name__ == "__main__":
+    if_train = False
+    if_search = True
     data_path = "data/train.txt"
+    pkl_filename = "pickle_model.pkl"
     # reader = Reader(line_format='user item rating timestamp', sep=',')
     # # 加载数据
     # data = Dataset.load_from_file(data_path, reader=reader)
@@ -21,15 +38,46 @@ if __name__ == "__main__":
     data = Dataset.load_from_df(ratings, reader)
     trainset, testset = train_test_split(data, test_size=.25)
     # model = SVDpp(n_epochs=1, verbose=True)
-    model = SVD(n_epochs=1, verbose=True)
-    model.fit(trainset)
-    predictions = model.test(testset)
-    # 然后计算RMSE
-    accuracy.rmse(predictions)
     # trainset = data.build_full_trainset()
     # model.train(trainset)
+    if if_search:
+        # 指定参数选择范围
+        param_grid = {'n_epochs': [10, 20], 'lr_all': [0.002, 0.005],
+                      'reg_all': [0.4, 0.6]}
 
-    # Save to file in the current working directory
-    pkl_filename = "pickle_model.pkl"
-    with open(pkl_filename, 'wb') as file:
-        pickle.dump(model, file)
+        gs = GridSearchCV(SVD, param_grid, measures=['rmse', 'mae'], cv=3)
+
+        gs.fit(data)
+
+        # 打印最好的均方根误差RMSE
+        print(gs.best_score['rmse'])
+
+        # 打印取得最好RMSE的参数集合
+        print(gs.best_params['rmse'])
+
+        # 现在可以使用产生最佳RMSE的算法
+        model = gs.best_estimator['rmse']
+        model.fit(data.build_full_trainset())
+        with open(pkl_filename, 'wb') as file:
+            pickle.dump(model, file)
+        
+    elif if_train:
+        model = SVD(n_epochs=1, verbose=True)
+        time_start = time.time()
+        model.fit(trainset)
+        elapsed = time.time() - time_start
+        print('Elapsed(train): %s' % (fmt_time(elapsed)))
+        # Save to file in the current working directory
+        predictions = model.test(testset)
+        # 然后计算RMSE
+        accuracy.rmse(predictions)
+        with open(pkl_filename, 'wb') as file:
+            pickle.dump(model, file)
+    else:
+        model = pickle.load(open(pkl_filename, 'rb'))
+        time_start = time.time()
+        predictions = model.test(testset)
+        elapsed = time.time() - time_start
+        print('Elapsed(predict): %s' % (fmt_time(elapsed)))
+        # 然后计算RMSE
+        accuracy.rmse(predictions)
