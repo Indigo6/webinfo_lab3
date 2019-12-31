428e0f8a4eca674c12145b6d968c302e90d4223d
clivefang <clivefang@tencent.com>
2019-12-31 20:11:18 +0800

    add predict module

--- SVD.py
+++ SVD.py
@@ -22,92 +22,97 @@ def fmt_time(dtime):
 
 
 if __name__ == "__main__":
-    if_train = True
+    if_train = False
     if_search = False
-    data_path = "data/train.txt"
+    train_data_path = "data/train.txt"
+    test_data_path = "data/test.txt"
     pkl_filename = "model.pkl"
-    # reader = Reader(line_format='user item rating timestamp', sep=',')
-    # # 加载数据
-    # data = Dataset.load_from_file(data_path, reader=reader)
-    data_type = {'user_id': np.int32, 'item_id': np.int32, 'rating': np.float32}
-    names = ['user_id', 'item_id', 'rating']
-    ratings = pd.read_csv(data_path, dtype=data_type, usecols=range(3), names=names)
-    print(type(ratings))
-    print(ratings.shape)
+    if if_train:
+        data_type = {'user_id': np.int32, 'item_id': np.int32, 'rating': np.float32}
+        names = ['user_id', 'item_id', 'rating']
+        ratings = pd.read_csv(train_data_path, dtype=data_type, usecols=range(3), names=names)
+        print(type(ratings))
+        print(ratings.shape)
 
-    reader = Reader(rating_scale=(0, 5))
-    data = Dataset.load_from_df(ratings, reader)
-    trainset, testset = train_test_split(data, test_size=.25)
-    # model = SVDpp(n_epochs=1, verbose=True)
-    # trainset = data.build_full_trainset()
-    # model.train(trainset)
-    if if_search:
-        # 跑不动，太慢了
-        # 指定参数选择范围
-        param_grid = {'n_epochs': [5, 10], 'lr_all': [0.002, 0.005],
-                      'reg_all': [0.4, 0.6]}
+        reader = Reader(rating_scale=(0, 5))
+        train_data = Dataset.load_from_df(ratings, reader)
+        trainset, validateset = train_test_split(train_data, test_size=.25)
+        if if_search:
+            # 跑不动，太慢了
+            # 指定参数选择范围
+            param_grid = {'n_epochs': [5, 10], 'lr_all': [0.002, 0.005],
+                          'reg_all': [0.4, 0.6]}
 
-        gs = GridSearchCV(SVD, param_grid, measures=['rmse', 'mae'], cv=5, n_jobs=-1, joblib_verbose=2)
+            gs = GridSearchCV(SVD, param_grid, measures=['rmse', 'mae'], cv=5, n_jobs=-1, joblib_verbose=2)
 
-        time_start = time.time()
-        gs.fit(data)
-        elapsed = time.time() - time_start
-        print('Elapsed(search best): %s' % (fmt_time(elapsed)))
-
-        results_df = pd.DataFrame.from_dict(gs.cv_results)
-        results_df.to_csv("grid_result.csv")
-        # 打印最好的均方根误差RMSE
-        print(gs.best_score['rmse'])
-
-        # 打印取得最好RMSE的参数集合
-        print(gs.best_params['rmse'])
-
-        # 现在可以使用产生最佳RMSE的算法
-        model = gs.best_estimator['rmse']
-        time_start = time.time()
-        model.fit(data.build_full_trainset())
-        elapsed = time.time() - time_start
-        print('Elapsed(best fit): %s' % (fmt_time(elapsed)))
-        with open(pkl_filename, 'wb') as file:
-            pickle.dump(model, file)
-        
-    elif if_train:
-        epoch = 20
-        total_epoch = 210
-        done_epoch = 0
-        time_start = time.time()
-        for i in range(1, epoch+1):
-            model = SVD(n_epochs=i, verbose=False)
-            model.fit(trainset)
-            # predictions_train = model.test(trainset)
-            predictions_test = model.test(testset)
-            # train_nmse = accuracy.rmse(predictions_train)
-            test_nmse = accuracy.rmse(predictions_test)
-            with open("model_e"+str(i)+".pkl", 'wb') as file:
-                pickle.dump(model, file)
+            time_start = time.time()
+            gs.fit(train_data)
             elapsed = time.time() - time_start
-            done_epoch += i
-            eta = (total_epoch - done_epoch) * elapsed / done_epoch if done_epoch > 0 else 0
-            # print('[%d/%d] Elapsed: %s, ETA: %s >> train_nmse:%s, test_nmse:%s' %
-            #       (i+1, epoch, fmt_time(elapsed), fmt_time(eta), train_nmse, test_nmse))
-            print('[%d/%d] Elapsed: %s, ETA: %s >> test_nmse:%s' %
-                  (i, epoch, fmt_time(elapsed), fmt_time(eta), test_nmse))
-            # print('Elapsed(train): %s' % (fmt_time(elapsed)))
+            print('Elapsed(search best): %s' % (fmt_time(elapsed)))
 
-        # Save to file in the current working directory
-        time_start = time.time()
-        predictions = model.test(testset)
-        elapsed = time.time() - time_start
-        print('Elapsed(predict): %s' % (fmt_time(elapsed)))
-        # 然后计算RMSE
-        accuracy.rmse(predictions)
-        with open(pkl_filename, 'wb') as file:
-            pickle.dump(model, file)
+            results_df = pd.DataFrame.from_dict(gs.cv_results)
+            results_df.to_csv("grid_result.csv")
+            # 打印最好的均方根误差RMSE
+            print(gs.best_score['rmse'])
+
+            # 打印取得最好RMSE的参数集合
+            print(gs.best_params['rmse'])
+
+            # 现在可以使用产生最佳RMSE的算法
+            model = gs.best_estimator['rmse']
+            time_start = time.time()
+            model.fit(train_data.build_full_trainset())
+            elapsed = time.time() - time_start
+            print('Elapsed(best fit): %s' % (fmt_time(elapsed)))
+            with open(pkl_filename, 'wb') as file:
+                pickle.dump(model, file)
+
+        else:
+            epoch = 20
+            total_epoch = 210
+            done_epoch = 0
+            time_start = time.time()
+            for i in range(1, epoch+1):
+                model = SVD(n_epochs=i, verbose=False)
+                model.fit(trainset)
+                # predictions_train = model.test(trainset)
+                predictions_test = model.test(validateset)
+                # train_nmse = accuracy.rmse(predictions_train)
+                test_nmse = accuracy.rmse(predictions_test)
+                with open("model_e"+str(i)+".pkl", 'wb') as file:
+                    pickle.dump(model, file)
+                elapsed = time.time() - time_start
+                done_epoch += i
+                eta = (total_epoch - done_epoch) * elapsed / done_epoch if done_epoch > 0 else 0
+                # print('[%d/%d] Elapsed: %s, ETA: %s >> train_nmse:%s, test_nmse:%s' %
+                #       (i+1, epoch, fmt_time(elapsed), fmt_time(eta), train_nmse, test_nmse))
+                print('[%d/%d] Elapsed: %s, ETA: %s >> test_nmse:%s' %
+                      (i, epoch, fmt_time(elapsed), fmt_time(eta), test_nmse))
+                # print('Elapsed(train): %s' % (fmt_time(elapsed)))
+
+            # Save to file in the current working directory
+            time_start = time.time()
+            predictions = model.test(validateset)
+            elapsed = time.time() - time_start
+            print('Elapsed(predict): %s' % (fmt_time(elapsed)))
+            # 然后计算RMSE
+            accuracy.rmse(predictions)
+            with open(pkl_filename, 'wb') as file:
+                pickle.dump(model, file)
     else:
-        model = pickle.load(open(pkl_filename, 'rb'))
+        data_type = {'user_id': np.int32, 'item_id': np.int32}
+        names = ['user_id', 'item_id']
+        test_data = pd.read_csv(train_data_path, dtype=data_type, usecols=range(2), names=names, nrows=100)
+        print(test_data.shape)
+
+        model = pickle.load(open("model_e10_old.pkl", 'rb'))
         time_start = time.time()
-        predictions = model.test(testset)
-        elapsed = time.time() - time_start
-        print('Elapsed(predict): %s' % (fmt_time(elapsed)))
+        length = test_data.shape[0]
+        for index, row in test_data.iterrows():
+            print(row['user_id'], row['item_id'])
+            predictions = model.predict(row['user_id'], row['item_id'])
+            print(predictions)
+            elapsed = time.time() - time_start
+            eta = (length - index - 1) * elapsed / (index + 1) if (index+1) > 0 else 0
+        print('[%d/%d] Elapsed: %s, ETA: %s ' % (index+1, length, fmt_time(elapsed), fmt_time(eta)))
         # 然后计算RMSE
-        accuracy.rmse(predictions)
